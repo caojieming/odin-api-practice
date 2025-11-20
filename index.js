933e21c0ccaf1762e123e5872ca7bd39ecf856d4
James Cao <jcao5360@gmail.com>
2025-11-20 15:42:11 -0500

    added async/await version of function

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 const searchInput = document.querySelector("#search-input");
 const searchBtn = document.querySelector("#search-btn");
 const img = document.querySelector("img");
-            
-// fetch("https://api.giphy.com/v1/gifs/translate?api_key=04eKjgolLioPm4hCES39hRYlsB3ZhNmA&s=cats")
-//     // first response = a promise (for the data we are looking for)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     // second response = the data we are looking for (returned from the previous promise)
-//     .then(function(response) {
-//         // gets the image url
-//         const cat = response.data.images.original.url
-//         console.log(cat);
-//         img.src = cat;
-//     });
+const API_KEY = "04eKjgolLioPm4hCES39hRYlsB3ZhNmA";
+
+
 
 searchBtn.addEventListener("click", searchForImg);
 
 function searchForImg() {
     const subject = searchInput.value;
     
-    fetch("https://api.giphy.com/v1/gifs/translate?api_key=04eKjgolLioPm4hCES39hRYlsB3ZhNmA&s=" + subject)
+    fetch("https://api.giphy.com/v1/gifs/translate?api_key=" + API_KEY + "&s=" + subject)
         // first response = a promise (for the data we are looking for)
         .then(function(response) {
             return response.json();
@@ -38,4 +28,29 @@ function searchForImg() {
         .catch(function(err) {
             console.log("Error: " + err);
         });
+}
+
+
+
+// same function, but made using await/async, generally neater looking
+async function searchForImg() {
+    try {
+        const subject = searchInput.value;
+
+        // response is a promise
+        const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=" + API_KEY + "&s=" + subject);
+        // imgData waits for + obtains json data from the promise
+        const imgData = await response.json();
+
+        // gets the image url
+        const imgURL = imgData.data.images.original.url
+        // console.log(imgURL);
+        img.src = imgURL;
+
+        // unhide img element
+        img.style.display = "block";
+    }
+    catch(err) {
+        console.log("Error: " + err);
+    }
 }
